feb95099da55bf30343ba363e527bf6a7083344b
tibraj <tibraj94@gmail.com>
2021-01-09 16:19:06 -0500

    sum function

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-document.getElementById('test').addEventListener('click', sum(range(1, 30)));
+document.getElementById('test').addEventListener('click', sumOfRange);
 
 function range(start, end, step = start < end ? 1 : -1) {
     let array = [];
@@ -8,4 +8,12 @@ function range(start, end, step = start < end ? 1 : -1) {
         for (let i = 0; i >= end; i += step) array.push(i)
     }
     return array;
+}
+
+function sum(array) {
+    let total = 0;
+    for(let value of array) {
+        total += value;
+    }
+    return total;
 }
