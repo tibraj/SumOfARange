148911c26888b24c39bbb70417982df3b353f995
tibraj <tibraj94@gmail.com>
2021-01-09 16:16:01 -0500

    range function

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-document.getElementById('test').addEventListener('click', sumOfRange);
+document.getElementById('test').addEventListener('click', sum(range(1, 30)));
+
+function range(start, end, step = start < end ? 1 : -1) {
+    let array = [];
+    if (step > 0) {
+        for (let i = 0; i <= end; i += step) array.push(i);
+    } else {
+        for (let i = 0; i >= end; i += step) array.push(i)
+    }
+    return array;
+}
